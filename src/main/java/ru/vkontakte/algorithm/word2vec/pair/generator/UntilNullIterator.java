67e31d314419f871392a2580f691dd8d8e063942
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2024-09-23 11:49:25 +0300

    fix

--- src/main/java/ru/vkontakte/algorithm/word2vec/pair/generator/UntilNullIterator.java
+++ src/main/java/ru/vkontakte/algorithm/word2vec/pair/generator/UntilNullIterator.java
@@ -8,16 +8,19 @@ import java.util.Iterator;
  * @author zamyatin-evg
  **/
 public abstract class UntilNullIterator<T> implements Iterator<T>, Serializable {
-    T next = generateOrNull();
+    T next = null;
 
     @Override
     public boolean hasNext() {
+        if (next == null) {
+            next = generateOrNull();
+        }
         return next != null;
     }
 
     @Override
     public T next() {
-        T result = next;
+        T result = next == null ? generateOrNull() : next;
         next = generateOrNull();
         return result;
     }
