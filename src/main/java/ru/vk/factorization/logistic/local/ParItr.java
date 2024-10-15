7d65d4ecbf039a8cf2f55e0a6f31e20b02203e30
Evgeny Zamyatin <e.zamyatin@corp.vk.com>
2024-10-15 15:52:46 +0300

    ParItr fix

--- src/main/java/ru/vk/factorization/logistic/local/ParItr.java
+++ src/main/java/ru/vk/factorization/logistic/local/ParItr.java
@@ -2,9 +2,10 @@ package ru.vk.factorization.logistic.local;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
@@ -12,69 +13,67 @@ import java.util.function.Consumer;
  * @author ezamyatin
  */
 public class ParItr {
-    public static <A> void foreach(Iterator<A> iterator, int cpus, Consumer<A> fn) {
-        LinkedBlockingQueue<A> inQueue = new LinkedBlockingQueue<A>(cpus * 5);
-        AtomicLong totalCounter = new AtomicLong(0);
-        Thread[] threads = new Thread[cpus];
-        AtomicReference<String> error = new AtomicReference<>(null);
 
-        for (int i = 0; i < cpus; ++i) {
-            threads[i] = new Thread((Runnable) () -> {
-                while (true) {
-                    A obj = null;
-                    try {
-                        obj = inQueue.take();
-                    } catch (InterruptedException ex) {
-                        break;
-                    } catch (Exception ex) {
-                        error.set(ex.getMessage());
-                        throw new RuntimeException(ex);
+    public static <A> void foreach(Iterator<A> iterator, int cpus, Consumer<A> fn) {
+        LinkedBlockingQueue<A> inQueue = new LinkedBlockingQueue<>(cpus * 5);
+
+        AtomicReference<Exception> error = new AtomicReference<>(null);
+        AtomicBoolean end = new AtomicBoolean(false);
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Thread[] threads = new Thread[cpus];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                try {
+                    while (!end.get() || !inQueue.isEmpty()) {
+                        fn.accept(inQueue.take());
                     }
-                    fn.accept(obj);
-                    totalCounter.decrementAndGet();
+                } catch (InterruptedException ignored){
+                } catch (Exception e) {
+                    error.set(e);
+                } finally {
+                    latch.countDown();
                 }
             });
-        }
-        Arrays.stream(threads).forEach(Thread::start);
+      }
 
-        iterator.forEachRemaining(e -> {
-            boolean ok = false;
-            while (!ok) {
-                if (error.get() != null) {
-                    throw new RuntimeException(error.get());
+        try {
+            Arrays.stream(threads).forEach(Thread::start);
+            iterator.forEachRemaining(e -> {
+                boolean ok = false;
+                while (!ok) {
+                    if (error.get() != null) {
+                        throw new RuntimeException(error.get());
+                    }
+
+                    try {
+                        ok = inQueue.offer(e, 1, TimeUnit.SECONDS);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
                 }
+            });
 
+            end.set(true);
+            if (!inQueue.isEmpty()) {
+                latch.await();
+            }
+
+            if (error.get() != null) {
+                throw error.get();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            Arrays.stream(threads).forEach(Thread::interrupt);
+            Arrays.stream(threads).forEach(e -> {
                 try {
-                    ok = inQueue.offer(e, 10, TimeUnit.SECONDS);
+                    e.join();
                 } catch (InterruptedException ex) {
                     throw new RuntimeException(ex);
                 }
-            }
-            totalCounter.incrementAndGet();
-        });
-
-        while (totalCounter.get() > 0) {
-            if (error.get() != null) {
-                throw new RuntimeException(error.get());
-            }
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                throw new RuntimeException(ex);
-            }
+            });
         }
-
-        try {
-            Arrays.stream(threads).forEach(Thread::interrupt);
-            for (int i = 0; i < threads.length; ++i) {
-                threads[i].join();
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        assert(totalCounter.get() == 0);
-        assert(!iterator.hasNext());
     }
 }
